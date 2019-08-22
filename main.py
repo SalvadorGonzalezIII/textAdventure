39b0bc23f5a30c97ea04f9850da07d2fe8c4236b
SalvadorGonzalezIII <sgonzalez371@toromail.csudh.edu>
2019-08-22 09:54:06 -0700

    page works

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import jinja2
 import os
 
+#from model import *
+
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -14,18 +16,24 @@ class MainPage(webapp2.RequestHandler):
 
 class pageOneHandler(webapp2.RequestHandler):
     def get(self):
+#question = questions(question =  "you are on island what do you do now?", answer1 = "stay on the beach", answer2)
         result_template = jinja_env.get_template('templates/pageOne.html')
         self.response.write(result_template.render())
 
-class deathPageHandler(webapp2.RequestHandler):
+class deathPageOneHandler(webapp2.RequestHandler):
     def get(self):
-        result_template = jinja_env.get_template('templates/deathPage.html')
+        result_template = jinja_env.get_template('templates/deathPageOne.html')
         self.response.write(result_template.render())
 
+class pageTwoHandler(webapp2.RequestHandler):
+    def get(self):
+        result_template = jinja_env.get_template('templates/pageTwo.html')
+        self.response.write(result_template.render())
 
 
 app = webapp2.WSGIApplication([
     ('/',MainPage),
     ('/pageOne',pageOneHandler),
-    ('/deathPage',deathPageHandler)
+    ('/deathPageOne',deathPageOneHandler),
+    ('/pageTwo',pageTwoHandler)
 ], debug=True)
---
+++ model.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import ndb
+class questions(ndb.Model):
+    """
+    question = StringProperty(required=True)
+    answer1 = StringProperty(required=True)
+    answer2 = StringProperty(required=True)
+    deathAnswer = StringProperty(required=True)
+    """
